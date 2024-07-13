cc1b69df95ad8d75291781a435f2096bcae370a8
mcombeau <mcombeau@student.42.fr>
2024-07-13 16:28:37 +0200

    refactor(dnsparser): move domain name parser function to utils

--- dnsparser/parser_question.go
+++ dnsparser/parser_question.go
@@ -24,29 +24,5 @@ func parseDNSQuestion(data []byte) (*DNSQuestion, int, error) {
 		QClass: parseUint16(data, offset+2),
 	}
 
-	return &question, offset, nil
-}
-
-/*
-Domain name in DNS encoded with labels, each label prefixed with length:
-[7, 'e', 'x', 'a', 'm', 'p', 'l', 'e', 3, 'c', 'o', 'm', 0]
-*/
-
-func parseDomainName(data []byte, offset int) (string, int) {
-	var name string
-	originalOffset := offset
-	for {
-		lenLabel := int(data[offset])
-		if lenLabel == 0 {
-			offset++
-			name += "." //for root domain
-			break
-		}
-		if len(name) > 0 {
-			name += "."
-		}
-		name += string(data[offset+1 : offset+1+lenLabel])
-		offset += lenLabel + 1
-	}
-	return name, offset - originalOffset
+	return &question, offset + 4, nil
 }
--- dnsparser/utils.go
+++ dnsparser/utils.go
@@ -22,3 +22,27 @@ hex:					0x12	 0x34		: 0x1234
 func parseUint16(data []byte, offset int) uint16 {
 	return uint16(data[offset])<<8 | uint16(data[offset+1])
 }
+
+/*
+Domain name in DNS encoded with labels, each label prefixed with length:
+[7, 'e', 'x', 'a', 'm', 'p', 'l', 'e', 3, 'c', 'o', 'm', 0]
+*/
+
+func parseDomainName(data []byte, offset int) (string, int) {
+	var name string
+	originalOffset := offset
+	for {
+		lenLabel := int(data[offset])
+		if lenLabel == 0 {
+			offset++
+			name += "." //for root domain
+			break
+		}
+		if len(name) > 0 {
+			name += "."
+		}
+		name += string(data[offset+1 : offset+1+lenLabel])
+		offset += lenLabel + 1
+	}
+	return name, offset - originalOffset
+}
