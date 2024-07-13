b403407db337f8e8fbfeb9488bf695b0bec20f6c
mcombeau <mcombeau@student.42.fr>
2024-07-13 16:59:40 +0200

    feat(dnsparser): add record name parsing

--- dnsparser/parser_record.go
+++ dnsparser/parser_record.go
@@ -1,12 +1,27 @@
 package dnsparser
 
-// DNSQuestion:
-//
+import "errors"
 
-// DNS RR:
-// Name string
-// Type uint16
-// Class uint16
-// TTL int32
-// RDLength uint16
-// RData []byte
+type DNSResourceRecord struct {
+	Name string
+	// Type     uint16
+	// Class    uint16
+	// TTL      int32
+	// RDLength uint16
+	// RData    []byte
+}
+
+func parseDNSResourceRecord(data []byte, offset int) (*DNSResourceRecord, int, error) {
+	name, newOffset := parseDomainName(data, offset)
+	offset += newOffset
+
+	if len(data) < offset+10 {
+		return &DNSResourceRecord{}, 0, errors.New("invalid DNS resource record")
+	}
+
+	record := DNSResourceRecord{
+		Name: name,
+	}
+
+	return &record, offset, nil
+}
--- dnsparser/parser_record_test.go
+++ dnsparser/parser_record_test.go
@@ -1,1 +1,33 @@
 package dnsparser
+
+import (
+	"testing"
+
+	"github.com/miekg/dns"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseResourceRecord(t *testing.T) {
+	mockResponse := mockDNSResponse()
+
+	var unpackedMockResponse dns.Msg
+	err := unpackedMockResponse.Unpack(mockResponse)
+
+	if err != nil {
+		t.Fatalf("Failed to unpack mock response: %v\n", err)
+	}
+
+	want := unpackedMockResponse.Answer[0]
+
+	_, offset, err := parseDNSQuestion(mockResponse)
+
+	if err != nil {
+		t.Fatalf("Failed to parse DNS question: %v\n", err)
+	}
+
+	got, _, err := parseDNSResourceRecord(mockResponse, offset)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, want.Header().Name, got.Name)
+}
