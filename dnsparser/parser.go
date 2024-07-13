28a704cf5ecece72dcfd49d1165a3c009e815a7b
mcombeau <mcombeau@student.42.fr>
2024-07-13 12:01:25 +0200

    feat(dnsparser): add transaction ID parsing

--- dnsparser/parser.go
+++ dnsparser/parser.go
@@ -1,2 +1,49 @@
 package dnsparser
 
+import "errors"
+
+// 12 bytes long
+// bytes 0-1: transaction ID
+// bytes 2-3: flags
+// bytes 4-5: Number of Questions
+// bytes 6-7: Number of Answer Resource Record (RR)
+// bytes 8-9: Number of Authority RRs
+// bytes 10-11: Number of Additional RRs
+type DNSHeader struct {
+	TransactionID uint16
+}
+
+func ParseDNSHeader(data []byte) (*DNSHeader, error) {
+	if len(data) < 12 {
+		return nil, errors.New("invalid DNS header")
+	}
+
+	header := DNSHeader{
+		TransactionID: parseTransactionID(data),
+	}
+
+	return &header, nil
+}
+
+/*
+parse header sections:
+"Concatenate" two bytes of header into int16.
+
+Ex. TransactionId = 1234:
+
+data[0]: 0x12:	00010010
+data[1]: 0x34:	00110100
+
+uint16(data[0]):	00000000 00010010
+uint16(data[1]):	00000000 00110100
+
+data[0] << 8:		00010010 00000000
+uint16(data[1]):	00000000 00110100
+|:					00010010 00110100
+
+hex:				0x12	 0x34		: 0x1234
+*/
+
+func parseTransactionID(data []byte) uint16 {
+	return uint16(data[0])<<8 | uint16(data[1])
+}
--- dnsparser/parser_mock_test.go
+++ dnsparser/parser_mock_test.go
@@ -1,1 +1,100 @@
 package dnsparser
+
+import (
+	"testing"
+
+	"github.com/miekg/dns"
+	"github.com/stretchr/testify/assert"
+)
+
+func mockDNSResponse() []byte {
+	msg := new(dns.Msg)
+	msg.SetReply(&dns.Msg{
+		MsgHdr: dns.MsgHdr{
+			Id:                 1234,
+			Response:           true,
+			Authoritative:      true,
+			RecursionAvailable: true,
+		},
+	})
+
+	msg.Question = []dns.Question{
+		{
+			Name:   "example.com.",
+			Qtype:  dns.TypeA,
+			Qclass: dns.ClassINET,
+		},
+	}
+
+	rr, _ := dns.NewRR("example.com. 3600 IN A 93.184.216.34")
+	msg.Answer = []dns.RR{rr}
+
+	response, _ := msg.Pack()
+	return response
+}
+
+func TestParseTransactionID(t *testing.T) {
+	mockResponse := mockDNSResponse()
+
+	var unpackedMockResponse dns.Msg
+	err := unpackedMockResponse.Unpack(mockResponse)
+
+	if err != nil {
+		t.Fatalf("Failed to unpack mock response: %v\n", err)
+	}
+
+	want := unpackedMockResponse.Id
+	got := parseTransactionID(mockResponse)
+
+	assert.Equal(t, want, got)
+}
+
+func TestParseHeader(t *testing.T) {
+	mockResponse := mockDNSResponse()
+
+	var unpackedMockResponse dns.Msg
+	err := unpackedMockResponse.Unpack(mockResponse)
+
+	if err != nil {
+		t.Fatalf("Failed to unpack mock response: %v\n", err)
+	}
+
+	want := unpackedMockResponse
+	got, err := ParseDNSHeader(mockResponse)
+
+	assert.NoError(t, err)
+	assert.Equal(t, want.Id, got.TransactionID)
+}
+
+// func viewtMockDNSResponse(t *testing.T) {
+// 	mockResponse := mockDNSResponse()
+
+// 	fmt.Println("------------------")
+
+// 	fmt.Printf("mock: %s\n", mockResponse)
+
+// 	fmt.Println("------------------")
+
+// 	for _, n := range mockResponse {
+// 		fmt.Printf("%08b ", n)
+// 	}
+
+// 	fmt.Println("\n------------------")
+
+// 	var msg dns.Msg
+// 	err := msg.Unpack(mockResponse)
+// 	if err != nil {
+// 		t.Fatalf("Failed to unpack mock response: %v\n", err)
+// 	}
+// 	fmt.Printf("Question domain: %s\n", msg.Question[0].Name)
+// 	fmt.Printf("Answer IP: %s\n", msg.Answer[0].(*dns.A).A.String())
+// 	fmt.Printf(msg.String())
+
+// 	fmt.Println("------------------")
+
+// 	// parsedResponse, err := ParseDNSResponse(mockResponse)
+
+// 	// assert.NoError(t, err)
+// 	// assert.Equal(t, "example.com.", parsedResponse.Question[0].Name)
+// 	// assert.Equal(t, "93.184.216.34", parsedResponse.Answer[0].(*dns.A).A.String())
+// }
