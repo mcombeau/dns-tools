901b189be4585b5e6d185475641f7e039d6eb925
mcombeau <mcombeau@student.42.fr>
2024-07-14 14:34:53 +0200

    feat(encode): add domain name encoding

---
+++ encoder/encode_domain_name_test.go
@@ -0,0 +1,40 @@
+package encoder
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncodeName(t *testing.T) {
+
+	tests := []struct {
+		data       string
+		wantBytes  []byte
+		wantLength int
+	}{
+		{
+			data:      "example.com",
+			wantBytes: []byte{7, 'e', 'x', 'a', 'm', 'p', 'l', 'e', 3, 'c', 'o', 'm', 0},
+		},
+		{
+			data:      "www.example.com",
+			wantBytes: []byte{3, 'w', 'w', 'w', 7, 'e', 'x', 'a', 'm', 'p', 'l', 'e', 3, 'c', 'o', 'm', 0},
+		},
+		{
+			data:      ".com",
+			wantBytes: []byte{3, 'c', 'o', 'm', 0},
+		},
+	}
+
+	for _, test := range tests {
+		var buf bytes.Buffer
+
+		encodeDomainName(&buf, test.data)
+		gotBytes := buf.Bytes()
+
+		assert.Equal(t, len(test.wantBytes), len(gotBytes))
+		assert.Equal(t, test.wantBytes, gotBytes)
+	}
+}
