f49ae69b38ccd5dda2be768b8e7d8bba2ec18cd2
mcombeau <mcombeau@student.42.fr>
2024-07-17 13:52:04 +0200

    refactor(test): cleanup tests + add test names

--- encoder/encode_domain_name_test.go
+++ encoder/encode_domain_name_test.go
@@ -10,30 +10,42 @@ import (
 func TestEncodeName(t *testing.T) {
 
 	tests := []struct {
+		name      string
 		data      string
 		wantBytes []byte
 	}{
 		{
+			name:      "Simple domain",
 			data:      "example.com",
 			wantBytes: []byte{7, 'e', 'x', 'a', 'm', 'p', 'l', 'e', 3, 'c', 'o', 'm', 0},
 		},
 		{
+			name:      "Subdomain",
 			data:      "www.example.com",
 			wantBytes: []byte{3, 'w', 'w', 'w', 7, 'e', 'x', 'a', 'm', 'p', 'l', 'e', 3, 'c', 'o', 'm', 0},
 		},
 		{
+			name:      "TLD",
 			data:      ".com",
 			wantBytes: []byte{3, 'c', 'o', 'm', 0},
+		},
+		{
+			name:      "Root domain",
+			data:      "",
+			wantBytes: []byte{0},
 		},
 	}
 
 	for _, test := range tests {
-		var buf bytes.Buffer
+		t.Run(test.name, func(t *testing.T) {
+			var buf bytes.Buffer
 
-		encodeDomainName(&buf, test.data)
-		gotBytes := buf.Bytes()
+			encodeDomainName(&buf, test.data)
+			gotBytes := buf.Bytes()
 
-		assert.Equal(t, len(test.wantBytes), len(gotBytes))
-		assert.Equal(t, test.wantBytes, gotBytes)
+			assert.Equal(t, len(test.wantBytes), len(gotBytes))
+			assert.Equal(t, test.wantBytes, gotBytes)
+
+		})
 	}
 }
